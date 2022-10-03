fe8cfaddef72b04c3ca0a14b97469c9c722e0c64
chazzlee <chazz@dev.com>
2022-10-03 13:54:55 -0400

    add new_moves_position

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -1,40 +1,46 @@
-require_relative "poly_tree_node.rb"
+require_relative 'poly_tree_node'
 
 class KnightPathFinder
+  def self.valid_moves(pos)
+    moves = []
 
-    def self.valid_moves(pos)
-
-        moves = []
-
-        [-2,2].each do |i|
-            [-1,1].each do |j|
-                new_pos = [pos[0] + i, pos[1] + j]
-                moves << new_pos
-                new_pos2 = [pos[0] + j, pos[1] + i]
-                moves << new_pos2
-            end
-        end
-        moves.select do |pos|
-            pos.all? {|i| i.between?(0,7)}
-        end
+    [-2, 2].each do |i|
+      [-1, 1].each do |j|
+        new_pos = [pos[0] + i, pos[1] + j]
+        moves << new_pos
+        new_pos2 = [pos[0] + j, pos[1] + i]
+        moves << new_pos2
+      end
     end
-
-    attr_reader :position
-
-    def  initialize(pos)
-        @position = pos
-        @root_node = PolyTreeNode.new(pos)
-        build_move_tree
+    moves.select do |new_pos|
+      new_pos.all? { |i| i.between?(0, 7) }
     end
+  end
 
-    def build_move_tree
+  attr_reader :position, :considered_positions
 
-    end
+  def initialize(pos)
+    @position = pos
+    @root_node = PolyTreeNode.new(pos)
+    @considered_positions = [pos]
+    build_move_tree
+  end
 
-    def find_path
+  def new_move_positions(pos)
+    valid_moves = KnightPathFinder.valid_moves(pos)
+    new_moves = valid_moves.select { |valid_move| !@considered_positions.include?(valid_move) }
+    considered_positions.concat(new_moves)
+    new_moves
+  end
 
-    end
-    
+  def build_move_tree; end
+
+  def find_path; end
 end
 
-p KnightPathFinder.valid_moves([0, 0])
+knight = KnightPathFinder.new([0, 0])
+p knight.considered_positions
+p knight.new_move_positions([0, 0])
+p knight.considered_positions
+
+# p KnightPathFinder.valid_moves([0, 0])
