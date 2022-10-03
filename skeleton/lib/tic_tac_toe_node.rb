33cce2bf45e4c93051017add41009553b4208dae
chazzlee <chazz@dev.com>
2022-10-03 16:47:27 -0400

    format

--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -1,7 +1,6 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-
   attr_reader :board, :next_mover_mark, :prev_move_pos
 
   def initialize(board, next_mover_mark, prev_move_pos = nil)
@@ -11,32 +10,32 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-    
+    if board.over?
+
+    end
   end
 
-  def winning_node?(evaluator)
-  end
+  def winning_node?(evaluator); end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-
     nodes = []
 
     (0...board.rows.length).each do |i|
       (0...board.rows.length).each do |j|
-        pos = [i,j]
-        if board.empty?(pos)
-          temp_board = board.dup
-          temp_board[pos] = next_mover_mark
-          next_mark = next_mover_mark == :x ? :o : :x
-          nodes << TicTacToeNode.new(temp_board, next_mark, pos)
-        end
+        pos = [i, j]
+        next unless board.empty?(pos)
+
+        temp_board = board.dup
+        temp_board[pos] = next_mover_mark
+        next_mark = next_mover_mark == :x ? :o : :x
+        nodes << TicTacToeNode.new(temp_board, next_mark, pos)
       end
     end
-  nodes
+    nodes
   end
 end
 
 p t = TicTacToeNode.new(Board.new, :x)
- t.children.each {|ele| p ele}
+t.children.each { |ele| p ele }
