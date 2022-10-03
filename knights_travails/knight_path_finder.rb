d07440d2be228e8e61b1feb8ffee897b729bcf49
Julian Cardone <juliancardone1@gmail.com>
2022-10-03 12:11:58 -0400

    valid_moved class

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -2,6 +2,23 @@ require_relative "poly_tree_node.rb"
 
 class KnightPathFinder
 
+    def self.valid_moves(pos)
+
+        moves = []
+
+        [-2,2].each do |i|
+            [-1,1].each do |j|
+                new_pos = [pos[0] + i, pos[1] + j]
+                moves << new_pos
+                new_pos2 = [pos[0] + j, pos[1] + i]
+                moves << new_pos2
+            end
+        end
+        moves.select do |pos|
+            pos.all? {|i| i.between?(0,7)}
+        end
+    end
+
     attr_reader :position
 
     def  initialize(pos)
@@ -19,3 +36,5 @@ class KnightPathFinder
     end
     
 end
+
+p KnightPathFinder.valid_moves([0, 0])
