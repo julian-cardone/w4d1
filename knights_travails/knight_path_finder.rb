f89bfb2dc4909cecb124be3bcd61feb1efca962d
Julian Cardone <juliancardone1@gmail.com>
2022-10-03 15:11:51 -0400

    finished!

--- knights_travails/knight_path_finder.rb
+++ knights_travails/knight_path_finder.rb
@@ -48,12 +48,29 @@ class KnightPathFinder
     end
   end
 
-  def find_path; end
+  def find_path(end_pos)
+
+    found_node = root_node.bfs(end_pos)
+    trace_path_back(found_node)
+
+  end
+
+  def trace_path_back(node)
+
+    return [node.value] if node.parent.nil?
+
+    trace_path_back(node.parent).push(node.value)
+
+  end
+
 end
 
-# knight = KnightPathFinder.new([0, 0])
+knight = KnightPathFinder.new([0, 0])
 
 # p knight
 # p knight.considered_positions
 # p knight.new_move_positions([0, 0])
 # p knight.considered_positions
+p knight.find_path([7,6])
+p knight.find_path([6,2])
+p 
